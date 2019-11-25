3dcc5a250f86a73fcb1ccdc57372b8b5cda097ba
gyul-akhmedova-av <anna.gyul-akhmedova@fxclub.com>
2019-11-25 17:33:29 +0700

    otbranchevalis'

--- AvroConsumerExpiration.py
+++ AvroConsumerExpiration.py
@@ -1,21 +1,36 @@
 from kafka import KafkaConsumer
 import AvroUtils
 
+# status = Notice - 1, Start - 2
+# kafka = dev или qa
+messagetype = 2
+kafka = 'qa'
 
-#кластер кафки
-kafka = '0.dual.kafka.qa-fxenv.com:9092','1.dual.kafka.qa-fxenv.com:9092','2.dual.kafka.qa-fxenv.com:9092'
-
-#Имя топика
-topic = 'expiration-service--symbolExpiration'
+if (kafka == 'qa'):
+    kafka = '0.dual.kafka.qa-fxenv.com:9092','1.dual.kafka.qa-fxenv.com:9092','2.dual.kafka.qa-fxenv.com:9092'
+    topic = 'expiration-service--symbolExpiration'
+elif(kafka == 'dev'):
+    kafka = '172.20.241.144:9092,172.20.244.128:9092,172.20.240.200:9092'
+    topic = 'expiration-service--symbolExpiration'
 
 consumer = KafkaConsumer(topic, bootstrap_servers=kafka)
 pathDll = './AvroUtils.dll'
-pathSchema = './avro_MT_templates/EXPIRATION_START.avsc'
+
+if (messagetype == 1):
+    pathSchema = './avro_MT_templates/EXPIRATION_START.avsc'
+    status = 'Start'
+elif(messagetype == 2):
+    pathSchema = './avro_MT_templates/EXPIRATION_NOTICE.avsc'
+    status = 'Notice'
+
 avroUtils = AvroUtils.JsonDeserializer(pathDll)
 
 for msg in consumer:
-    print('Avro: ' + msg.value.decode())
+    #print('Avro: ' + msg.value.decode())
+    print('Avro: ')
+    print(msg.value)
     try:
-        print('Json: ' + avroUtils.fromAvroBinaryBase64(pathSchema,msg.value).decode() + '\n')
+        #print('Json: ' + avroUtils.fromAvroBinaryBase64(pathSchema,msg.value).decode() + '\n')
+        print('Json: ' + avroUtils.fromAvroBinary(pathSchema,msg.value).decode() + '\n')
     except Exception:
         print('Not valid message. Skip.')
--- AvroProducerExpiration.py
+++ AvroProducerExpiration.py
@@ -1,25 +1,47 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 import AvroUtils
+# status = Notice - 1, Start - 2
+# kafka = dev или qa
+messagetype = 2
+kafka = 'qa'
 
 #создание экземпляра авро
 pathDll = './AvroUtils.dll'
-pathSchema = './avro_MT_templates/EXPIRATION_START.avsc'
+
+if (messagetype == 1):
+    pathSchema = './avro_MT_templates/EXPIRATION_START.avsc'
+    status = 'Start'
+elif(messagetype == 2):
+    pathSchema = './avro_MT_templates/EXPIRATION_NOTICE.avsc'
+    status = 'Notice'
+
 avroUtils = AvroUtils.JsonSerializer(pathDll)
 
 
 #кластер кафки
-kafka = '0.dual.kafka.qa-fxenv.com:9092','1.dual.kafka.qa-fxenv.com:9092','2.dual.kafka.qa-fxenv.com:9092'
-#Имя топика
-topic = 'expiration-service--symbolExpiration'
+if (kafka == 'qa'):
+    kafka = '0.dual.kafka.qa-fxenv.com:9092','1.dual.kafka.qa-fxenv.com:9092','2.dual.kafka.qa-fxenv.com:9092'
+    topic = 'expiration-service--symbolExpiration'
+elif(kafka == 'dev'):
+    kafka = '172.20.241.144:9092,172.20.244.128:9092,172.20.240.200:9092'
+    topic = 'expiration-service--symbolExpiration'
 
 #исходное сообщение
-message ='{"symbol": "TestA","dateTime": "2019-11-06T03:27:00","expirationId": 1,"lastContract": "TestA9V","newContract": "TestA9X"}'
-print('Json: ' + message)
-key = '1234'.encode()
+message ='{"symbol":"BRN","dateTime":"2019-11-25T09:17:00","expirationId":2226,"lastContract":"BRN0H","newContract":"BRN1C"}'
 
-avroMessage = avroUtils.toAvroBinaryBase64(pathSchema,message.encode())
-print('Avro: ' + avroMessage.decode())
+print('Json: ' + message)
+#key = '1234'.encode()
+key = ''.encode()
+
+headers = [('MESSAGE_TYPE', str.encode('ENTITY_EVENT')), ('ENTITY_TYPE', str.encode('symbolExpiration')),
+           ('ENTITY_STATUS', str.encode(status)), ('MESSAGE_PRODUCER', str.encode('ExpirationService'))]
+
+avroMessage = avroUtils.toAvroBinary(pathSchema,message.encode())
+
+#print('Avro: ' + avroMessage.decode())
+print('Avro: ')
+print(avroMessage)
 
 def on_send_success(record_metadata):
     print(record_metadata.topic)
@@ -29,12 +51,13 @@ def on_send_success(record_metadata):
 def on_send_error(excp):
     print('I am an errback', exc_info=excp)
 
-def Send_message(topic, message, key):
+def Send_message(topic, message, key,headers):
     producer = KafkaProducer(bootstrap_servers=kafka)
-    future = producer.send(topic, message, key).add_callback(on_send_success).add_errback(on_send_error)
+    future = producer.send(topic, message, key,headers).add_callback(on_send_success).add_errback(on_send_error)
+
     try:
         record_metadata = future.get(timeout=10)
     except KafkaError:
         pass
 
-Send_message(topic,avroMessage,key)
+Send_message(topic,avroMessage,key,headers)
